3055302509b21c14cd8fa17dc30b1788017b87a5
joelharr <joelharr@umich.edu>
2019-09-09 15:38:00 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+import random
+print("Joel Harrison")
+one = random.randint(0,100)
+two = random.randint(0,100)
+print(one)
+print (two)
+print("Sum = ", one + two)
+print("Average = ", (one + two) / 2)
